7fe1bc5eea0bbc774c8e7c991a8580458e18f315
ec3938 <ec3938@nyu.edu>
2022-11-03 23:12:39 -0400

    Added dragging from tutorial video

---
+++ Assets/Scripts/FloatyBox.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatyBox : MonoBehaviour
+{
+    // Start is called before  the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0){
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Moved)
+            {
+                _rigidbody.AddForce(touch.deltaPosition);
+            }
+        }
+    }
+}
