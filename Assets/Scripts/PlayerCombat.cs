15c9a8602116f50fb2a12bdc7c80806c39020097
Sabahat-Sami <70985974+Sabahat-Sami@users.noreply.github.com>
2022-11-11 16:06:43 -0500

    Tried to fix lerp

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -20,6 +20,9 @@ public class PlayerCombat : MonoBehaviour
     }
 
     public void takeHit(){
-        enemy.transform.position = Vector2.Lerp(enemy.transform.position, transform.position/2, Time.deltaTime);
+        print("hit");
+        print(enemy.transform.position);
+        enemy.transform.position = Vector2.Lerp(enemy.transform.position, new Vector2(transform.position.x, -2.429525f), 0.5f);
+        print(enemy.transform.position);
     }
 }
