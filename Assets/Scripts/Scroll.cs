090ad7a7fbc6f9a27bf1a98ffb0d99985cfffc5f
stiztics <il2149@nyu.edu>
2022-11-17 21:30:37 -0500

    Change object movement from physics to position

--- Assets/Scripts/Scroll.cs
+++ Assets/Scripts/Scroll.cs
@@ -7,11 +7,12 @@ public class Scroll : MonoBehaviour
     Renderer _renderer;
     Rigidbody _rigidbody;
     float curr_time;
+    public Material _mat;
 
     //public float speed = .1f;
     void Start()
     {
-        _renderer = GetComponent<Renderer>();
+        //_renderer = GetComponent<Renderer>();
         _rigidbody = GetComponent<Rigidbody>();
     }
 
@@ -20,7 +21,10 @@ public class Scroll : MonoBehaviour
         if(PublicVars.speed < 0.75f && PublicVars.speed !=0f)
         {
             PublicVars.speed = PublicVars.speed + PublicVars.accel;
+            PublicVars.objectSpeed = PublicVars.speed * PublicVars.image_offset * Time.deltaTime;
         }
-        _renderer.material.mainTextureOffset += new Vector2(1, 0) * PublicVars.speed * Time.deltaTime;
+        //_renderer.material.mainTextureOffset += new Vector2(1, 0) * PublicVars.speed * Time.deltaTime;
+        
+        _mat.mainTextureOffset += new Vector2(1, 0) * PublicVars.speed * Time.deltaTime;
     }
 }
--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -8,20 +8,24 @@ public class Wall : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
-        _rigidbody.velocity = new Vector2(-PublicVars.speed * PublicVars.image_offset, 0);
+        //_rigidbody = GetComponent<Rigidbody2D>();
+        //_rigidbody.velocity = new Vector2(-PublicVars.speed * PublicVars.image_offset, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        _rigidbody.velocity = new Vector2(-PublicVars.speed * PublicVars.image_offset, 0);
+        //_rigidbody.velocity = new Vector2(-PublicVars.speed * PublicVars.image_offset, 0);
+        Vector2 newPos = transform.position;
+        newPos.x -= PublicVars.objectSpeed;
+        transform.position = newPos;
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player")
         {
             PublicVars.speed = 0f;
+            PublicVars.objectSpeed = 0f;
         }
     }
 }
