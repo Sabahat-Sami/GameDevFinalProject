ea51c970d609f630d035ab47edcf8b6a7afa2fc8
Sabahat-Sami <70985974+Sabahat-Sami@users.noreply.github.com>
2022-11-17 21:36:22 -0500

    Added score multiplier power up

--- Assets/Scripts/PublicVars.cs
+++ Assets/Scripts/PublicVars.cs
@@ -7,5 +7,6 @@ public static class PublicVars
     public static float speed = .1f;
     public static float accel = .0002f;
     public static float image_offset = 21.62746f;
-
+    public static float maxScore = 0;
+    public static float scoreAdder = 100;
 }
