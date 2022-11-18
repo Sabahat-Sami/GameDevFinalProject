b620dd03f25284a38115cdad63be4970c80ad273
stiztics <il2149@nyu.edu>
2022-11-17 23:34:39 -0500

    Fixed multiplier powerup bugs

--- Assets/Scripts/ScoreMultiplier.cs
+++ Assets/Scripts/ScoreMultiplier.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class ScoreMultiplier : MonoBehaviour
 {
+    void Update()
+    {
+        Vector2 newPos = transform.position;
+        newPos.x -= PublicVars.objectSpeed;
+        transform.position = newPos;
+    }
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player") {
             StartCoroutine(multiplyScore());
+            gameObject.GetComponent<CircleCollider2D>().enabled = false;
+            gameObject.GetComponent<Renderer>().enabled = false;
         }
     }
     IEnumerator multiplyScore(){
         PublicVars.scoreAdder = 500;
         yield return new WaitForSeconds(5f);
         PublicVars.scoreAdder = 100;
+        Destroy(this.gameObject);
     }
 }
