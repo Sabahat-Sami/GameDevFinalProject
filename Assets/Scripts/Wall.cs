a2c774f1aad5ca8fd1bb2b4b06afd403581d7078
stiztics <il2149@nyu.edu>
2022-12-02 08:27:23 -0500

    Added wall hit sound

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -6,9 +6,14 @@ using UnityEngine;
 public class Wall : MonoBehaviour
 {
     Rigidbody2D _rigidbody;
+    AudioSource _audioSource;
+
+    public AudioClip hitWall;
+
     // Start is called before the first frame update
     void Start()
     {
+        _audioSource = GetComponent<AudioSource>();
         //_rigidbody = GetComponent<Rigidbody2D>();
         //_rigidbody.velocity = new Vector2(-PublicVars.speed * PublicVars.image_offset, 0);
     }
@@ -25,6 +30,7 @@ public class Wall : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player")
         {
+            _audioSource.PlayOneShot(hitWall);
             PublicVars.speed = 0f;
             PublicVars.objectSpeed = 0f;
             StartCoroutine(Death());
@@ -33,7 +39,7 @@ public class Wall : MonoBehaviour
 
     IEnumerator Death()
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.8f);
         SceneManager.LoadScene("PlayAgain");
     }
 }
